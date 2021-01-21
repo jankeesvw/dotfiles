a9d0a61d73b570e2be5af22e19f3372a23e286b5
Jankees <mail@jankeesvw.com>
2021-01-21 08:56:09 +0100

    Script to toggle light at my office door

--- bin/toggle_busy_light.sh
+++ bin/toggle_busy_light.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl -X GET 'http://192.168.2.42:8080/json.htm?type=command&param=switchlight&idx=111&switchcmd=Toggle'
+curl -X GET 'http://192.168.1.2:8080/json.htm?type=command&param=switchlight&idx=111&switchcmd=Toggle'
+
+open "swiftbar://refreshplugin?name=busy"
